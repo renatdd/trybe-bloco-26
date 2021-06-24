8b22de4dbbaf2c4ae7f1a5384dd1d62ae5f995b3
Renato Feitosa <pythaqua@gmail.com>
2021-06-24 19:21:03 -0300

    feat(getUserByName): Add case insensitive search

--- middlewares/getPostById.js
+++ middlewares/getPostById.js
@@ -3,7 +3,7 @@ const posts = require('../data/posts');
 const getPostById = (req, res, next) => {
   const { id } = req.params;
   const foundPost = posts.find((post) => post.id === Number(id));
-  if (!foundPost) return next(new Error('id not found'));
+  if (!foundPost) throw new Error('id not found');
   res.status(200).send(foundPost);
 };
 
--- middlewares/getUserByName.js
+++ middlewares/getUserByName.js
@@ -2,7 +2,7 @@ const users = require('../data/users');
 
 const getUserByName = (req, res, next) => {
   const { name } = req.params;
-  const foundUser = users.find((user) => user.user === name);
+  const foundUser = users.find(({ user }) => user.match(new RegExp(name, 'i')));
   if (!foundUser) return next(new Error('user not found'));
   res.status(200).send({ comments: foundUser.comments });
 };
